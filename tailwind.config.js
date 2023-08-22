30eb8f0ccea08a2588b8cabde6c57f83762ef589
Michael Bard <michael1bard@gmail.com>
2023-08-21 23:17:44 -0400

    product page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
     "./sections/*.liquid",
     "./snippets/*.liquid",
     "./templates/customers/*.liquid",
-    "./templates/*.liquid"
+    "./templates/*.liquid",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        94: "22rem",
+      },
+    },
   },
   plugins: [],
 };
